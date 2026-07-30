dbdc5dbf5d121b8cce2fc4a609d7d5ec494458c4
MartinSurgeon <martinsurgeon@users.noreply.github.com>
2026-07-30 00:53:19 +0000

    feat: create central configuration file and update contact form to route leads to new primary email constant

--- contact.php
+++ contact.php
@@ -178,8 +178,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             log_submission_event('WARNING', 'Database object $db is not initialized (email will still send)');
         }
 
-        // Direct all form leads to primary SITE_EMAIL (info@rmgroupstrategies.com)
-        $to = SITE_EMAIL;
+        // Direct all form leads to primary mailbox (erm@rmgroupstrategies.com)
+        $to = defined('EMAIL_ERM') && !empty(EMAIL_ERM) ? EMAIL_ERM : 'erm@rmgroupstrategies.com';
         $subject = "New Website Lead: " . ucwords(str_replace('_', ' ', $inquiry_type));
         
         // Format email body
